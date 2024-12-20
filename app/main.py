f711d959a993a0d16aa84cb6eb0c5559af796072
JulianskyMe <juliakamynskaya@gmail.com>
2024-12-20 15:37:41 +0200

    UPD

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        self.wife = None
+        self.husband = None
 
         Person.people[name] = self
 
@@ -12,13 +14,11 @@ def create_person_list(people: list) -> list:
     for person in people:
         name = person["name"]
         age = person["age"]
-
-        person_instance = Person(name, age)
-
-        Person.people[name] = person_instance
-
+        Person(name, age)  
+        
     for person in people:
         current_instance = Person.people[person["name"]]
+
         if person.get("wife"):
             current_instance.wife = Person.people[person["wife"]]
         
